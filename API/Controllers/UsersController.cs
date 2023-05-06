40979ef1114a2c7804c41662344db8d6b9b616d8
PARZIVAL27082002 <devanshuhar1@gmail.com>
2023-05-06 11:27:14 +0530

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,24 +5,28 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 namespace API.Controllers
 {        
-    [Authorize]
+    
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
     public IUserRepository _userRepository { get; }
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
       _userRepository = userRepository;
-           
+           _mapper = mapper;
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             var users = await _userRepository.GetMembersAsync();
@@ -30,10 +34,27 @@ namespace API.Controllers
             return Ok(users);
         }
 
+        [Authorize]
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
              return await _userRepository.GetMemberAsync(username);
+        }
+
+        
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
---
+++ client/src/app/member/member-card/member-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MemberCardComponent implements OnInit {
+  @Input() member: Member | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
